b7de70c102dd4f23a321921c1f9af1cec8d73c67
avgoncharov <andrey.goncharov@kaspersky.com>
2017-09-20 19:46:40 +0300

    Start.

---
+++ src/Math/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Math
+{
+	public struct Point
+	{
+		public DateTimeOffset DateTimeOffset { get; }
+		public double Value { get; }
+
+		public Point(DateTimeOffset date, double value)
+		{
+			DateTimeOffset = date;
+			Value = value;
+		}
+		
+	}
+}
---
+++ src/Math/QuadraticInterpolation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Math
+{
+	public class QuadraticInterpolation
+	{
+		private Point[] _sourcePoints;
+		public QuadraticInterpolation(Point[] points)
+		{
+			_sourcePoints = points.ToArray();
+		}
+
+		public Point[] Interpolate(DateTimeOffset[] dates)
+		{
+			return new Point[0];
+		}
+	}
+}
